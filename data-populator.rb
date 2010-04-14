b3183f866edd6ff937c895a6cbe76d06a8bcf1bd
hyfather <hyfather@hoplite.(none)>
2010-04-14 14:27:12 +0530

    First Cut of Overseer!

---
+++ data-populator.rb
@@ -0,0 +1,65 @@
+require "rubygems"
+require "open-uri"
+require "active_support"
+
+HUDSON_JOBS_TO_MONITOR = ['mmh', 'mmh_stress_release', 'mmh_hotfix_release', 'mmh-indexer', 'mmh-indexer-branch-hotfix'] 
+MML_URLS_TO_MONITOR = ['mmhtestqa.kih.kmart.com', 'mmhbuild02p.ecom.sears.com:8180', 'mmh-stress1.ecom.sears.com', 'www.managemylife.com']
+
+ENVIRONMENTS_DATA_FILE = "./ENV_DASHBOARD_DATA"
+HUDSON_JOBS_DATA_FILE = "./BUILD_STATUS_DATA"
+
+def translate_color(hudson_job_color)
+  {"blue" => "green", "blue_anime" => "green building", "red" => "red", "red_anime" => "red building", "yellow" => "yellow", "yellow_anime" => "yellow building"}[hudson_job_color]
+end
+
+
+# process hudson jobs
+jobs = ""
+begin
+  outer_json = open("http://mmhbuild01p.ecom.sears.com:8080/hudson/api/json").read
+  outer_hash = ActiveSupport::JSON.decode(outer_json)
+
+
+
+  outer_hash["jobs"].each do |job|
+    if HUDSON_JOBS_TO_MONITOR.include? job["name"]
+      url = "http://mmhbuild01p.ecom.sears.com:8080/hudson/job/#{job["name"]}/lastBuild/api/json"
+      last_build_json = open(url).read
+      last_build_hash = ActiveSupport::JSON.decode(last_build_json)
+
+
+      commit_message = (last_build_hash["changeSet"]["items"].try(:first))
+      commit_message = commit_message.nil? ? "No Changeset. Build was triggered manually or by upstream!" : commit_message["msg"]
+      commit_message.gsub!(/,|<|>|\//, ' ')
+
+      jobs << [job["name"], translate_color(job["color"]), last_build_hash["number"], commit_message].join(",")
+      jobs << "\n"
+
+    end
+  end
+rescue Exception => e
+  p e.inspect
+  jobs = ""
+end
+
+File.new(HUDSON_JOBS_DATA_FILE, "w").print jobs
+
+
+
+# process environments data file.
+environments = ""
+MML_URLS_TO_MONITOR.each do |url|
+  begin
+    all_properties = open("http://" + url + "/mmh/version.properties").read
+    csv_string = ([url, all_properties.split(/\n|=/)[1], all_properties.split(/\n|=/)[3]].join(",") + "\n")
+  rescue
+    csv_string = url + ",-,-,DEAD\n"
+  end
+
+  environments << csv_string
+end
+
+File.new(ENVIRONMENTS_DATA_FILE, "w").print environments
+
+
+
---
+++ overseer.rb
@@ -0,0 +1,68 @@
+require 'rubygems'
+require 'sinatra'
+require 'active_support'
+
+ENVIRONMENTS_DATA_FILE="./ENV_DASHBOARD_DATA"
+HUDSON_JOBS_DATA_FILE="./BUILD_STATUS_DATA"
+
+set :public, File.dirname(__FILE__) + '/views'
+set :port, 10000
+
+get '/' do
+  @environments = []
+  @environments_last_updated = timeago(File.mtime(ENVIRONMENTS_DATA_FILE))
+  environments_data = File.new(ENVIRONMENTS_DATA_FILE, "r")
+  environments_data.each_line do |line|
+    values = line.chomp.split(/,/)
+    @environments << {:name => values[0], :build => values[1], :branch => values[2], :dead => (values[3] == 'DEAD')}
+  end
+
+  @jobs = []
+  @hudson_last_updated = timeago(File.mtime(HUDSON_JOBS_DATA_FILE))
+  hudson_jobs_data = File.new(HUDSON_JOBS_DATA_FILE, "r")
+  hudson_jobs_data.each_line do |line|
+    values = line.split(/,/)
+    @jobs << {:name => values[0], :status => values[1], :build_number => values[2], :commit_msg => values[3]}
+  end
+
+  erb :dashboard
+end
+
+# helper methods
+
+def timeago(time, options = {})
+   start_date = options.delete(:start_date) || Time.new
+   date_format = options.delete(:date_format) || :default
+   delta_minutes = (start_date.to_i - time.to_i).floor / 60
+   if delta_minutes.abs <= (8724*60)
+     distance = distance_of_time_in_words(delta_minutes)
+     if delta_minutes < 0
+        return "#{distance} from now"
+     else
+        return "#{distance} ago"
+     end
+   else
+      return "on #{DateTime.now.to_formatted_s(date_format)}"
+   end
+end
+
+ def distance_of_time_in_words(minutes)
+   case
+     when minutes < 1
+       "less than a minute"
+     when minutes == 1
+       "#{minutes} minute"
+     when minutes < 50
+       "#{minutes} minutes"
+     when minutes < 90
+       "about one hour"
+     when minutes < 1080
+       "#{(minutes / 60).round} hours"
+     when minutes < 1440
+       "one day"
+     when minutes < 2880
+       "about one day"
+     else
+       "#{(minutes / 1440).round} days"
+   end
+ end
