3590fa392138e9becf9815ca7140ac3a3454e1f2
hyfather <hyfather@hoplite.(none)>
2010-04-21 16:19:32 +0530

    adding startup scripts to repo.

---
+++ start_data-populator.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+export PS1=overseer && . /home/env8/.bashrc && cd /projects/overseer/ && cruby data-populator.rb
---
+++ start_overseer.sh
@@ -0,0 +1,1 @@
+( source ~/.bashrc && cruby overseer.rb -o 10.12.1.122) > /dev/null 2>&1 
